9ae961f5255f6ac34a7a53e9df5de66f3483b5c0
galicians <galicians@gmail.com>
2014-11-23 02:34:06 +0000

    all views added

--- app/scripts/app.config.js
+++ app/scripts/app.config.js
@@ -1,9 +1,37 @@
 
 
 angular.module('stackOverTubeApp').config(function($stateProvider) {
+  $stateProvider
 
-  $stateProvider.state('allvideos', {
+    .state('askquestion', {
+    url: '/askquestion',
+    templateUrl: '/views/askquestion.html'
+  })
+
+  .state('tags', {
+    url: '/tags',
+    templateUrl: '/views/tags.html'
+  })
+
+  .state('allvideos', {
     url: '/allvideos',
     templateUrl: '/views/allvideos.html'
   })
+
+   .state('unanswered', {
+    url: '/unanswered',
+    templateUrl: '/views/unanswered.html'
+  })
+
+  .state('register', {
+    url: '/register',
+    templateUrl: '/views/register.html'
+  })
+
+  .state('aboutus', {
+    url: '/aboutus',
+    templateUrl: '/views/aboutus.html'
+  })
+
+
 })
