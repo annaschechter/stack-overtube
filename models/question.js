3c3d81bb46fbf9286588bbc73c0940570ded8265
Anna Jerofejeva <jerofejeva.anna@yahoo.com>
2014-11-24 19:11:54 +0000

    Added psql db with sequelize

---
+++ models/question.js
@@ -0,0 +1,16 @@
+module.exports = function(sequelize, DataTypes) {
+	var Question = sequelize.define('Question', {
+	  title: DataTypes.STRING,
+	  description: DataTypes.STRING,
+	  codeSnippet: DataTypes.STRING,
+	  githubRepo: DataTypes.STRING
+	}, {
+		classMethods: {
+			associate: function(models) {
+				Question.hasMany(models.Reply);
+			}
+		}
+	});
+
+	return Question;
+};
