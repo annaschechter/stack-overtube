70e9f8d03cf2353aee0b5a2482e8cb4369835992
Anna Jerofejeva <jerofejeva.anna@yahoo.com>
2014-11-24 13:39:29 +0000

    Created an empty node.js app

---
+++ server.js
@@ -0,0 +1,14 @@
+var app = require('express')();
+var server = require('http').createServer(app);
+
+app.set('view engine', 'ejs');
+app.set('views',__dirname + '/views');
+
+
+app.get('/', function(req, res) {
+	res.send('<h1>StackOverTube</h1>')
+})
+
+server.listen(3000, function() {
+	console.log('listening on port 3000');
+});
