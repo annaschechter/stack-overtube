a147a722db8bacb4fd590223bb0f132346edcf75
Anna Jerofejeva <jerofejeva.anna@yahoo.com>
2014-11-25 12:31:24 +0000

    Saving questions to the db

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 var app = require('express')();
 var server = require('http').createServer(app);
+var Question = require('./models/question.js');
+var bodyParser = require('body-parser');
+var models = require('./models');
 
-
-app.set('view engine', 'ejs');
-app.set('views',__dirname + '/views');
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended: true
+}))
 
 app.get('/', function(req, res) {
-	res.send('<h1>StackOverTube</h1>')
-})
+	res.render('index');
+});
+
+app.post('/askquestion', function(req, res) {
+	var question = req.body;
+		console.log(req.body);
+	models.Question.create({ title: question.title, description: question.description, codeSnippet: question.codeSnippet, githubRepo: question.githubRepo });
+
+});
 
 module.exports = app
