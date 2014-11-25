321dd5bf8483d2b87d9d34ea6b9f1bc1a20b3552
Anna Jerofejeva <jerofejeva.anna@yahoo.com>
2014-11-25 13:37:55 +0000

    Can access a specific question

--- server.js
+++ server.js
@@ -17,7 +17,15 @@ app.post('/askquestion', function(req, res) {
 	var question = req.body;
 		console.log(req.body);
 	models.Question.create({ title: question.title, description: question.description, codeSnippet: question.codeSnippet, githubRepo: question.githubRepo });
+});
 
+app.get('/question/:id', function(req, res) {
+	var id = req.url.toString().split('/').slice(-1)[0];
+	console.log(id);
+	var currentQuestion = models.Question.find( {where:{id: id}} ).complete(function(err, question) {
+		console.log(question.title);
+		console.log(question.description);
+	});
 });
 
 module.exports = app
